6944da0f89cc6e5c72d492f86370131764cbf53a
Emmanuel Béziat <emmanuel@italic.fr>
2015-05-22 15:24:34 +0200

    v2.0: jquery prototype

--- src/js/jquery-modern-form.js
+++ src/js/jquery-modern-form.js
@@ -1,50 +1,95 @@
 /*!
  * A simple handler for forms that add classes on your forms, so you can play with CSS.
- * Version : 1.0
- * Par Emmanuel "Manumanu" B. (www.emmanuelbeziat.com)
+ * Version : 2.0
+ * Emmanuel B. (www.emmanuelbeziat.com)
  * https://github.com/EmmanuelBeziat/jquery-modern-form
  **/
 
-;(function($, undefined) {
-	"use strict";
+;(function($, window, document, undefined) {
+	'use strict';
 
-	$.fn.modernForm = function(params) {
-
-		// Valeurs par défauts des options
-		params = $.extend({
+	/**
+	 * Default values
+	 */
+	var pluginName = 'modernForm',
+		defaults = {
 			inputSelector: '.form-input',
 			classFocus: 'form-group--focus',
 			classLabel: 'form-group--label'
-		}, params);
+		};
 
-		// Actions du plugin
-		this.each(function() {
+	/**
+	 * Constructor
+	 */
+	var Plugin = function(element, options) {
+		this.element = element;
 
-			// Variables
-			var $input = $(this).find(params.inputSelector);
+		this.settings = $.extend({}, defaults, options);
+
+		this._defaults = defaults;
+		this._name = pluginName;
+
+		this.init();
+	};
+
+	/**
+	 * Methods
+	 */
+	$.extend(Plugin.prototype, {
+
+		init: function() {
+			var plugin = this,
+				settings = plugin.settings,
+				$input = $(plugin.element).find(settings.inputSelector);
 
 			$input
-				.on('focus', function() {
-					$(this).parent().addClass(params.classFocus + ' ' + params.classLabel);
-				})
-				.on('blur', function() {
-					var $parent = $(this).parent();
+				.on({
+					'focus': function() {
+						plugin.focusInput();
+					},
 
-					if ($(this).val() === '') {
-						$parent.removeClass(params.classLabel);
+					'blur': function() {
+						plugin.blurInput();
 					}
-
-					$parent.removeClass(params.classFocus);
+				})
+				.each(function() {
+					if ($(this).val() !== '') {
+						$(this).parent().addClass(settings.classLabel);
+					}
 				});
+		},
 
-			$input.each(function() {
-				if ($(this).val() !== '') {
-					$(this).parent().addClass(params.classLabel);
-				}
-			});
+		/**
+		 * When an item get the focus
+		 */
+		focusInput: function() {
+			$(event.target).parent().addClass(this.settings.classFocus + ' ' + this.settings.classLabel);
+		},
+
+		/**
+		 * When an item lose the focus
+		 */
+		blurInput: function() {
+			var $parent = $(event.target).parent();
+
+			if ($(event.target).val() === '') {
+				$parent.removeClass(this.settings.classLabel);
+			}
+
+			$parent.removeClass(this.settings.classFocus);
+		}
+	});
+
+	/**
+	 * jQuery plugin wrapper
+	 */
+	$.fn[pluginName] = function(options) {
+
+		return this.each(function() {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+			}
 		});
 
-		// Chainage jQuery
-		return this;
 	};
-})(jQuery);
+})(jQuery, window, document);
