8fdc38ba6878d7e2c86c169a28056253a958630a
Emmanuel Béziat <emmanuel@italic.fr>
2017-06-30 01:06:30 +0200

    update

--- src/js/izzi-modern-form.js
+++ src/js/izzi-modern-form.js
@@ -1,6 +1,6 @@
 /*!
  * A simple handler for forms that add classes on your forms, so you can play with CSS.
- * Version : 2.0.9
+ * Version : 2.0.10
  * Emmanuel B. (www.emmanuelbeziat.com)
  * https://github.com/EmmanuelBeziat/izzi-modern-form
  **/
